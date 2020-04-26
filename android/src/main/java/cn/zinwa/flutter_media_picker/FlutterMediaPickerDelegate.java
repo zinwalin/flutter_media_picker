ef4c5e3969343f5ad43a1eaa529551fb94734a5b
linzhenhua <linzhenhua@zhongch.onaliyun.com>
2020-04-26 14:32:20 +0800

    upgrade PictureSelector to v.2.5.3

--- android/src/main/java/cn/zinwa/flutter_media_picker/FlutterMediaPickerDelegate.java
+++ android/src/main/java/cn/zinwa/flutter_media_picker/FlutterMediaPickerDelegate.java
@@ -59,13 +59,12 @@ public class FlutterMediaPickerDelegate implements PluginRegistry.ActivityResult
         PictureWindowAnimationStyle mWindowAnimationStyle = new PictureWindowAnimationStyle();
         mWindowAnimationStyle.ofAllAnimation(R.anim.picture_anim_up_in, R.anim.picture_anim_down_out);
 
-        // TODO .maxSelectNum()
-
-        PictureSelector.create(this.activity).openGallery(chooseMode)
+        PictureSelector.create(this.activity).openGallery(chooseMode).isPageStrategy(true)
                 .setPictureWindowAnimationStyle(mWindowAnimationStyle).isWeChatStyle(true).recordVideoSecond(30)
                 .enableCrop(false).freeStyleCropEnabled(false).compress(true).compressQuality(60).maxVideoSelectNum(1)
                 .maxSelectNum(maxSelectCount).isOriginalImageControl(true).rotateEnabled(true).scaleEnabled(true)
-                .imageFormat(PictureMimeType.PNG).loadImageEngine(GlideEngine.createGlideEngine())
+                .isAutomaticTitleRecyclerTop(true).isMaxSelectEnabledMask(true).imageFormat(PictureMimeType.PNG)
+                .loadImageEngine(GlideEngine.createGlideEngine())
                 // .forResult(PictureConfig.CHOOSE_REQUEST);
                 .forResult(new OnResultCallbackListener<LocalMedia>() {
                     @Override
